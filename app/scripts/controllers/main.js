5b948ce9862e0915bc422bb9d1fcdc84bda7f854
王金鑫 <mhplala@gmail.com>
2016-01-15 14:07:21 +0800

    init program

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name btpanUiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the btpanUiApp
+ */
+angular.module('btpanUiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
